6b49c02dc8c9d2b780160af0ab1c9987bc657fe3
Tomasz Mentel <mentell@o2.pl>
2019-09-06 19:54:58 +0200

    updated url trello

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://stark-atoll-69760.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://stark-atoll-69760.herokuapp.com/v1/task/trello/';
+  const trelloApiRoot = 'https://stark-atoll-69760.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
